51ba5121192111f136a35565b40a852a20c817fa
Georg Svetlov <gsvetlov@gmail.com>
2021-06-15 13:26:20 +0300

    basic app setup

---
+++ cloud-client/src/main/java/ru/svetlov/storage/client/MainApp.java
@@ -0,0 +1,22 @@
+package ru.svetlov.storage.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ru.svetlov.storage.client.controller.MainWindowController;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("view/MainWindow.fxml"));
+        Parent parent = loader.load();
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.setTitle("Cloud-storage client");
+        primaryStage.setResizable(true);
+        MainWindowController controller = loader.getController();
+        primaryStage.setOnCloseRequest((event) -> controller.shutdown());
+        primaryStage.show();
+    }
+}
---
+++ cloud-client/src/main/java/ru/svetlov/storage/client/controller/MainWindowController.java
@@ -0,0 +1,21 @@
+package ru.svetlov.storage.client.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+
+    public void shutdown() {
+
+    }
+}
